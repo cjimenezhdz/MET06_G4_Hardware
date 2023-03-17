aa3dd2fbbb09c1b6eb7502e9a1815fde8d0249fa
cjimenezhdz <cecilia.jh.22@gmail.com>
2023-03-17 18:05:10 +0100

    new tab: test_temp_hum

--- main/main.ino
+++ main/main.ino
@@ -4,8 +4,9 @@ void setup() {
 
   Serial.begin(9600);
   //presence_setup();
-  temperature_setup();
-  panic_setup();
+  //temperature_setup();
+  //panic_setup();
+  temp_hum_setup();
 
 } 
 
@@ -13,11 +14,12 @@ void loop() {
 
   //presence_loop();
   if (temp_cont == 5){    //monitorización cada 5seg
-    temperature_loop();
+    temp_hum_loop();
     temp_cont = 0;
   }
   temp_cont ++;
   
   //panic_loop();
+  
 
 }
