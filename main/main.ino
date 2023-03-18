67051871ee65a3766921502f22a6f7bfdc62fd3f
cjimenezhdz <cecilia.jh.22@gmail.com>
2023-03-18 21:14:20 +0100

    servomotor integration

--- main/main.ino
+++ main/main.ino
@@ -4,13 +4,14 @@ void setup() {
 
   Serial.begin(9600);
   //presence_setup();
- panic_setup();
+  panic_setup();
   temp_hum_setup();
+  servo_setup();
 
 } 
 
 void loop() { 
-
+  
   //presence_loop();
   if (temp_cont == 400000){    //monitorización cada 5seg, fcloclk es de 80 MHz(T=12,5ns) y queremos un T=5s)
       temp_hum_loop();
@@ -19,6 +20,6 @@ void loop() {
   temp_cont ++;
   
   panic_loop();
-  
+  servo_loop();
 
 }
