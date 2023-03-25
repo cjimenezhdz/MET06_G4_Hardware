201ceeeae2a7bb11ae1b6e60f23f884773cd6ae2
CarlosCalpe12 <126815924+CarlosCalpe12@users.noreply.github.com>
2023-03-25 19:59:28 +0100

    update main

--- main/main.ino
+++ main/main.ino
@@ -3,23 +3,23 @@ int temp_cont = 0;
 void setup() { 
 
   Serial.begin(9600);
-  //presence_setup();
+  presence_setup();
   panic_setup();
   temp_hum_setup();
-  servo_setup();
+  //servo_setup();
 
 } 
 
 void loop() { 
   
-  //presence_loop();
+  
+  panic_loop();
   if (temp_cont == 400000){    //monitorización cada 5seg, fcloclk es de 80 MHz(T=12,5ns) y queremos un T=5s)
       temp_hum_loop();
       temp_cont = 0;
   }
   temp_cont ++;
+  presence_loop();
+  //servo_loop();
   
-  panic_loop();
-  servo_loop();
-
 }
