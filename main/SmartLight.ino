1d39d6d328010481d3aac313d70ca97057520264
CarlosCalpe12 <126815924+CarlosCalpe12@users.noreply.github.com>
2023-03-25 21:44:56 +0100

    update: logica button led

---
+++ main/SmartLight.ino
@@ -0,0 +1,81 @@
+
+const int button = 10;
+const int ledButton = 12;
+int buttonStateLight = 0;
+int buttonModeLight = 0;
+
+const int LightSensor = 0;
+const int ledLightSensor = 4;
+int LightState = 0;
+int LightMode = 0;
+
+void SmartLight_setup() { 
+ 
+  pinMode(button, INPUT);
+  pinMode(ledButton, OUTPUT);
+  pinMode(LightSensor, INPUT);
+  pinMode(ledLightSensor, OUTPUT);
+  
+} 
+
+void SmartLight_loop() { 
+
+  buttonStateLight = digitalRead(button);
+
+  if (buttonStateLight == HIGH) {
+    buttonModeLight++;
+    if (buttonModeLight > 3) {
+      buttonModeLight = 0;
+    }
+    delay(1000);
+  }
+
+
+  switch (buttonModeLight) {
+    case 0:                           
+      analogWrite(ledButton, 0);
+      Serial.println("Modo1");
+      break;
+    case 1:                           
+      analogWrite(ledButton, 50);
+      Serial.println("Modo2");
+      break;
+    case 2:                           
+      analogWrite(ledButton, 150);
+      Serial.println("Modo3");
+      break;
+    case 3:                          
+      analogWrite(ledButton, 250);
+      Serial.println("Modo4");
+      break;      
+  }
+
+ /* LightState = digitalRead(LightSensor);
+
+
+  if (LightState == HIGH) {
+    LightMode++;
+    if (LightMode > 3) {
+      LightMode = 0;
+    }
+    delay(1000);
+  }
+
+
+  switch (LightMode) {
+    case 0:                           
+      analogWrite(LightMode, 0);
+      break;
+    case 1:                           
+      analogWrite(LightMode, 50);
+      break;
+    case 2:                           
+      analogWrite(LightMode, 150);
+      break;
+    case 3:                          
+      analogWrite(LightMode, 250);
+      break;      
+  }
+*/
+  
+}
