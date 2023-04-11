10f8d30a45c41b79bdd4cbe29aa72aecd2cc888b
CarlosCalpe12 <126815924+CarlosCalpe12@users.noreply.github.com>
2023-04-11 17:12:48 +0200

    loop implementation on each branch

--- main/test_servomotor.ino
+++ main/test_servomotor.ino
@@ -15,6 +15,8 @@ int servoMode = 0;
 
 const int ledYELLOW = 4; 
 
+boolean bucle=false;
+
 void servo_setup() {
   Grifo.attach(ServoPin);                           // attaches the servo to SERVOPIN
   pinMode(buttonServoPin, INPUT);
@@ -23,50 +25,53 @@ void servo_setup() {
 }
 
 void servo_loop() {
+
+  while(!bucle){
+    
+    buttonServoState = digitalRead(buttonServoPin);
+    buttonServoEmergencyState = digitalRead(buttonServoEmergencyPin);
   
-  buttonServoState = digitalRead(buttonServoPin);
-  buttonServoEmergencyState = digitalRead(buttonServoEmergencyPin);
-
-  if (buttonServoEmergencyState == HIGH) {
-    servoMode = 0;
-    Grifo.write(servoMode);
-    analogWrite(ledYELLOW, 100);
-    Serial.println("EMERGENCY");
-    return;
-  }
-
-  if (buttonServoState == HIGH) {
-    servoMode++;
-    if (servoMode > 2) {
+    if (buttonServoEmergencyState == HIGH) {
       servoMode = 0;
+      Grifo.write(servoMode);
+      analogWrite(ledYELLOW, 100);
+      Serial.println("EMERGENCY");
+      return;
     }
-    delay(1000);
-  }
-
-  switch (servoMode) {
-    case 0:                           //GRIFO CERRADO
-      servoPos = 0;
-      analogWrite(ledYELLOW, 0);
-      break;
-    case 1:                           //GRIFO CAUDAL MEDIO
-      servoPos = 90;
-      analogWrite(ledYELLOW, 50);
-      delay(200);
-      analogWrite(ledYELLOW, 0);
-      delay(200);
-      break;
-    case 2:                           //GRIFO CAUDAL ALTO
-      servoPos = 180;
-      analogWrite(ledYELLOW, 255);
-      delay(100);
-      analogWrite(ledYELLOW, 0);
-      delay(100);
-      break;
-  }
-
-  Grifo.write(servoPos);
-  delay(15);
   
+    if (buttonServoState == HIGH) {
+      servoMode++;
+      if (servoMode > 2) {
+        servoMode = 0;
+      }
+      delay(1000);
+    }
+  
+    switch (servoMode) {
+      case 0:                           //GRIFO CERRADO
+        servoPos = 0;
+        analogWrite(ledYELLOW, 0);
+        break;
+      case 1:                           //GRIFO CAUDAL MEDIO
+        servoPos = 90;
+        analogWrite(ledYELLOW, 50);
+        delay(200);
+        analogWrite(ledYELLOW, 0);
+        delay(200);
+        break;
+      case 2:                           //GRIFO CAUDAL ALTO
+        servoPos = 180;
+        analogWrite(ledYELLOW, 255);
+        delay(100);
+        analogWrite(ledYELLOW, 0);
+        delay(100);
+        break;
+    }
+  
+    Grifo.write(servoPos);
+    delay(15);
+
+  }
 } 
     
     
