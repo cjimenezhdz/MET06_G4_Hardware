03e6931af204c76c8cf455bb277ac1369e787dc2
CarlosCalpe12 <126815924+CarlosCalpe12@users.noreply.github.com>
2023-04-12 18:02:11 +0200

    fix logic

--- main/test_presence_detection.ino
+++ main/test_presence_detection.ino
@@ -1,8 +1,10 @@
-int pirPinDown =5; 
-int pirStateDown=0;
-int pirPinUp =14; 
+int pirPinUp=5; 
 int pirStateUp=0;
-float sensorDistance=0.5;
+
+int pirPinDown=13; 
+int pirStateDown=0;
+
+float sensorDistance=0.25;
 float fallingTime=sqrt((2*sensorDistance)/9.8);
 
 void presence_setup() { 
@@ -11,38 +13,27 @@ void presence_setup() {
 } 
 
 void presence_loop() { 
-
-  while(true){
     
+  while(true){
+
     pirStateUp = digitalRead(pirPinUp); //read state of the PIR 
-  
-    if (pirStateUp == HIGH) { 
-         delay(fallingTime);
-    }
-       /* Serial.println("UP Motion!"); //if the value read is low, there  was no motion 
-        
-    } else { 
+    pirStateDown = digitalRead(pirPinDown); //read state of the PIR
+    
+    if (pirStateUp == HIGH && pirStateDown != HIGH) { 
+      
+     delay(fallingTime);
+     Serial.println("UP Motion!"); //if the value read is low, there  was no motion 
+          
+     pirStateDown = digitalRead(pirPinDown); //read state of the PIR
+          if(pirStateDown==HIGH){
+          Serial.println("----------CAIDA DETECTADA------------");
+          delay(5000);
+          }
+    }else { 
       
       Serial.println("UP No motion"); //if the value read was high, there was motion 
-    } */
-    
-   
-    pirStateDown = digitalRead(pirPinDown); //read state of the PIR
-  
-    /*
-    if (pirStateDown == HIGH) { 
-        
-        Serial.println("DOWNMotion!"); //if the value read is low, there  was no motion 
-        
-    } else { 
-      
-     Serial.println("Down No motion"); //if the value read was high, there was motion 
-    }*/
-  
-    if(pirStateUp==HIGH && pirStateDown==HIGH){
-      Serial.println("Caida Detectada");
     }
-    
+
   }
-  
+
 }
